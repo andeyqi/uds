258a32514c1da8c7bbc213e2dcb20cb030e1d8bb
andeyqi <qihengchuan8888@163.com>
2020-06-16 15:16:28 +0800

    增加安全访问算法

--- uds_test.py
+++ uds_test.py
@@ -42,14 +42,14 @@ def recive_thread_func():
                       rcv_msg[i].frame.eff, rcv_msg[i].frame.rtr,
                       ''.join(str(rcv_msg[i].frame.data[j]) + ' ' for j in range(rcv_msg[i].frame.can_dlc))))
         elif rcv_canfd_num:
-            print("Receive CANFD message number:%d" % rcv_canfd_num)
+            #print("Receive CANFD message number:%d" % rcv_canfd_num)
             rcv_canfd_msgs, rcv_canfd_num = zcanlib.ReceiveFD(chn_handle, rcv_canfd_num, 1000)
-            for i in range(rcv_canfd_num):
-                print("[%d]:ts:%d, id:%d, len:%d, eff:%d, rtr:%d, esi:%d, brs: %d, data:%s" %(
-                        i, rcv_canfd_msgs[i].timestamp, rcv_canfd_msgs[i].frame.can_id, rcv_canfd_msgs[i].frame.len,
-                        rcv_canfd_msgs[i].frame.eff, rcv_canfd_msgs[i].frame.rtr, 
-                        rcv_canfd_msgs[i].frame.esi, rcv_canfd_msgs[i].frame.brs,
-                        ''.join(str(rcv_canfd_msgs[i].frame.data[j]) + ' ' for j in range(rcv_canfd_msgs[i].frame.len))))
+            #for i in range(rcv_canfd_num):
+                #print("[%d]:ts:%d, id:%d, len:%d, eff:%d, rtr:%d, esi:%d, brs: %d, data:%s" %(
+                #        i, rcv_canfd_msgs[i].timestamp, rcv_canfd_msgs[i].frame.can_id, rcv_canfd_msgs[i].frame.len,
+                #        rcv_canfd_msgs[i].frame.eff, rcv_canfd_msgs[i].frame.rtr, 
+                #        rcv_canfd_msgs[i].frame.esi, rcv_canfd_msgs[i].frame.brs,
+                #        ''.join(str(rcv_canfd_msgs[i].frame.data[j]) + ' ' for j in range(rcv_canfd_msgs[i].frame.len))))
         #else:
         #    break
 
@@ -59,19 +59,38 @@ class SEED(Structure):
                 ("seed3", c_ubyte),
                 ("seed4", c_ubyte)]
 
+class KEY(Structure):
+    _fields_ = [("key1", c_ubyte),
+                ("key2", c_ubyte),
+                ("key3", c_ubyte),
+                ("key4", c_ubyte),
+                ("key5", c_ubyte),
+                ("key6", c_ubyte),
+                ("key7", c_ubyte),
+                ("key8", c_ubyte),
+                ("key9", c_ubyte),
+                ("key10", c_ubyte),
+                ("key11", c_ubyte),
+                ("key12", c_ubyte),
+                ("key13", c_ubyte),
+                ("key14", c_ubyte),
+                ("key15", c_ubyte),
+                ("key16", c_ubyte)]
+
 def shell_command():
-    test = windll.LoadLibrary("test.dll")
+    secureAccess = windll.LoadLibrary("secureAccess.dll")
     seed = SEED()
     seed.seed1 = 1
     seed.seed2 = 2
     seed.seed3 = 3
     seed.seed4 = 4
-    seedhash = SEED()
+    key = KEY()
     while True:
         s = input("C71KB#")
         print("%s" %(s))
-        print("%d" %(seed.seed1))
-        test.test(byref(seed),byref(seedhash))
+        secureAccess.security_key(byref(seed),byref(key))
+        print("%x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x " %(key.key1, key.key2, key.key3, key.key4, key.key5, key.key6, key.key7, key.key8,
+        key.key9, key.key10, key.key11, key.key12, key.key13, key.key14, key.key15, key.key16))
 
 if __name__ == "__main__":
     zcanlib = ZCAN() 
