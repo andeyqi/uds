025bbb77ac7b7e70a12529681de3823d2b3d239b
andeyqi <qihengchuan8888@163.com>
2020-06-16 20:12:56 +0800

    简易版本代码

--- uds_test.py
+++ uds_test.py
@@ -1,6 +1,37 @@
 from zlgcan import *
 import threading
 import ctypes
+import os
+from multiprocessing import Process, Lock
+import time
+
+
+class SEED(Structure):
+    _fields_ = [("seed1", c_ubyte),
+                ("seed2", c_ubyte),
+                ("seed3", c_ubyte),
+                ("seed4", c_ubyte)]
+
+class KEY(Structure):
+    _fields_ = [("key", c_ubyte*16)]
+
+
+class GINFO(Structure):
+    _fields_ = [("is_session_pass", c_ubyte),
+                ("is_get_seed",     c_ubyte),
+                ("seed",            SEED),
+                ("key",            KEY)]
+
+
+ginfo = GINFO()
+ginfo.is_session_pass = 0
+
+secureAccess = windll.LoadLibrary("secureAccess.dll")
+
+def data_deinit():
+    ginfo.is_session_pass = 0
+    ginfo.is_get_seed = 0
+
 #import numpy as np
 
 def can_start(zcanlib, device_handle, chn):
@@ -27,70 +58,108 @@ def can_start(zcanlib, device_handle, chn):
     zcanlib.StartCAN(chn_handle)
     return chn_handle
 
+def deal_service_resp(rcv_canfd_msgs):
+    print("recv resp")
+    if rcv_canfd_msgs.frame.data[1] == 0x50:
+        ginfo.is_session_pass = 1
+    if rcv_canfd_msgs.frame.data[1] == 0x67:
+        ginfo.seed.seed1 = rcv_canfd_msgs.frame.data[3]
+        ginfo.seed.seed2 = rcv_canfd_msgs.frame.data[4]
+        ginfo.seed.seed3 = rcv_canfd_msgs.frame.data[5]
+        ginfo.seed.seed4 = rcv_canfd_msgs.frame.data[6]
+        ginfo.is_get_seed = 1
+    event.set()
+
 def recive_thread_func():
     #Receive Messages
     while True:
-        rcv_num = zcanlib.GetReceiveNum(chn_handle, ZCAN_TYPE_CAN)
+        mutex.acquire()
         rcv_canfd_num = zcanlib.GetReceiveNum(chn_handle, ZCAN_TYPE_CANFD)
-        #print("num %d  can fd num %d."%(rcv_num,rcv_canfd_num))
-        if rcv_num:
-            print("Receive CAN message number:%d" % rcv_num)
-            rcv_msg, rcv_num = zcanlib.Receive(chn_handle, rcv_num)
-            for i in range(rcv_num):
-                print("[%d]:ts:%d, id:%d, dlc:%d, eff:%d, rtr:%d, data:%s" %(i, rcv_msg[i].timestamp, 
-                      rcv_msg[i].frame.can_id, rcv_msg[i].frame.can_dlc, 
-                      rcv_msg[i].frame.eff, rcv_msg[i].frame.rtr,
-                      ''.join(str(rcv_msg[i].frame.data[j]) + ' ' for j in range(rcv_msg[i].frame.can_dlc))))
-        elif rcv_canfd_num:
-            #print("Receive CANFD message number:%d" % rcv_canfd_num)
+        if rcv_canfd_num:
             rcv_canfd_msgs, rcv_canfd_num = zcanlib.ReceiveFD(chn_handle, rcv_canfd_num, 1000)
-            #for i in range(rcv_canfd_num):
-                #print("[%d]:ts:%d, id:%d, len:%d, eff:%d, rtr:%d, esi:%d, brs: %d, data:%s" %(
-                #        i, rcv_canfd_msgs[i].timestamp, rcv_canfd_msgs[i].frame.can_id, rcv_canfd_msgs[i].frame.len,
-                #        rcv_canfd_msgs[i].frame.eff, rcv_canfd_msgs[i].frame.rtr, 
-                #        rcv_canfd_msgs[i].frame.esi, rcv_canfd_msgs[i].frame.brs,
-                #        ''.join(str(rcv_canfd_msgs[i].frame.data[j]) + ' ' for j in range(rcv_canfd_msgs[i].frame.len))))
+            for i in range(rcv_canfd_num):
+                if rcv_canfd_msgs[i].frame.can_id == 0x79a:
+                    print("recv 0x79a msg [%x] [%x] [%x] [%x] [%x] [%x]" %(rcv_canfd_msgs[i].frame.data[0],rcv_canfd_msgs[i].frame.data[1],rcv_canfd_msgs[i].frame.data[2],rcv_canfd_msgs[i].frame.data[3],rcv_canfd_msgs[i].frame.data[4],rcv_canfd_msgs[i].frame.data[5]))
+                    deal_service_resp(rcv_canfd_msgs[i])
+        mutex.release()
         #else:
         #    break
 
-class SEED(Structure):
-    _fields_ = [("seed1", c_ubyte),
-                ("seed2", c_ubyte),
-                ("seed3", c_ubyte),
-                ("seed4", c_ubyte)]
-
-class KEY(Structure):
-    _fields_ = [("key1", c_ubyte),
-                ("key2", c_ubyte),
-                ("key3", c_ubyte),
-                ("key4", c_ubyte),
-                ("key5", c_ubyte),
-                ("key6", c_ubyte),
-                ("key7", c_ubyte),
-                ("key8", c_ubyte),
-                ("key9", c_ubyte),
-                ("key10", c_ubyte),
-                ("key11", c_ubyte),
-                ("key12", c_ubyte),
-                ("key13", c_ubyte),
-                ("key14", c_ubyte),
-                ("key15", c_ubyte),
-                ("key16", c_ubyte)]
-
+def service_0x27():
+    print("uds 0x27 service")
+    #step 1 set 诊断会话控制 01 默认 02 编程 03 扩展
+    canfd_msgs = ZCAN_TransmitFD_Data()
+    canfd_msgs.transmit_type = 1 #Send Self
+    canfd_msgs.frame.eff     = 0 #extern frame
+    canfd_msgs.frame.rtr     = 0 #remote frame
+    canfd_msgs.frame.brs     = 1 #BRS 
+    canfd_msgs.frame.can_id  = 0x792
+    canfd_msgs.frame.len     = 8
+    canfd_msgs.frame.data[0] = 0x02
+    canfd_msgs.frame.data[1] = 0x10
+    canfd_msgs.frame.data[2] = 0x03
+    mutex.acquire()
+    ret = zcanlib.TransmitFD(chn_handle, canfd_msgs, 1)
+    print("Tranmit CANFD Num: %d. %x" %(ret,canfd_msgs.frame.can_id))
+    mutex.release()
+    event.wait()
+    event.clear()
+    if ginfo.is_session_pass == 0:
+        return 
+    print("Set session OK ,%d" % ginfo.is_session_pass)   
+    #step 2 get seed data
+    canfd_msgs.transmit_type = 1 #Send Self
+    canfd_msgs.frame.eff     = 0 #extern frame
+    canfd_msgs.frame.rtr     = 0 #remote frame
+    canfd_msgs.frame.brs     = 1 #BRS 
+    canfd_msgs.frame.can_id  = 0x792
+    canfd_msgs.frame.len     = 8
+    canfd_msgs.frame.data[0] = 0x02
+    canfd_msgs.frame.data[1] = 0x27
+    canfd_msgs.frame.data[2] = 0x01
+    mutex.acquire()
+    ret = zcanlib.TransmitFD(chn_handle, canfd_msgs, 1)
+    print("Tranmit CANFD Num: %d. %x" %(ret,canfd_msgs.frame.can_id))
+    mutex.release()
+    event.wait()
+    event.clear()
+    if ginfo.is_get_seed == 0:
+        print("get seed NG")
+        return
+    print("get seed ok")
+    secureAccess.security_key(byref(ginfo.seed),byref(ginfo.key))
+    #step 3 send key value
+    canfd_msgs.transmit_type = 1 #Send Self
+    canfd_msgs.frame.eff     = 0 #extern frame
+    canfd_msgs.frame.rtr     = 0 #remote frame
+    canfd_msgs.frame.brs     = 1 #BRS 
+    canfd_msgs.frame.can_id  = 0x792
+    canfd_msgs.frame.len     = 20
+    canfd_msgs.frame.data[0] = 0x00
+    canfd_msgs.frame.data[1] = 18
+    canfd_msgs.frame.data[2] = 0x27
+    canfd_msgs.frame.data[3] = 0x02
+    for i in range (0,16):
+        print("% d %d " % ( i , ginfo.key.key[i]))
+        canfd_msgs.frame.data[4+i] = ginfo.key.key[i]
+    mutex.acquire()
+    ret = zcanlib.TransmitFD(chn_handle, canfd_msgs, 1)
+    print("Tranmit CANFD Num: %d. %x" %(ret,canfd_msgs.frame.can_id))
+    mutex.release()
+    event.wait()
+    data_deinit()
+    #step 4 wait compare key value
+    
 def shell_command():
-    secureAccess = windll.LoadLibrary("secureAccess.dll")
-    seed = SEED()
-    seed.seed1 = 1
-    seed.seed2 = 2
-    seed.seed3 = 3
-    seed.seed4 = 4
-    key = KEY()
     while True:
         s = input("C71KB#")
+        if s == "0x27":
+            service_0x27()
         print("%s" %(s))
-        secureAccess.security_key(byref(seed),byref(key))
-        print("%x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x " %(key.key1, key.key2, key.key3, key.key4, key.key5, key.key6, key.key7, key.key8,
-        key.key9, key.key10, key.key11, key.key12, key.key13, key.key14, key.key15, key.key16))
+        #secureAccess.security_key(byref(seed),byref(key))
+        #print("%x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x " %(key.key1, key.key2, key.key3, key.key4, key.key5, key.key6, key.key7, key.key8,
+        #key.key9, key.key10, key.key11, key.key12, key.key13, key.key14, key.key15, key.key16))
+
 
 if __name__ == "__main__":
     zcanlib = ZCAN() 
@@ -99,17 +168,16 @@ if __name__ == "__main__":
         print("Open Device failed!")
         exit(0)
     print("device handle:%d." %(handle))
-
+    
     info = zcanlib.GetDeviceInf(handle)
     print("Device Information:\n%s" %(info))
-
+    mutex = Lock()
     #Start CAN
     chn_handle = can_start(zcanlib, handle, 0)
     print("channel handle:%d." %(chn_handle))
-    
     recv_thread = threading.Thread(group=None, target=recive_thread_func, name="recv_thread")
     recv_thread.start()
-    
+    event = threading.Event()
     shell_thread = threading.Thread(group=None, target=shell_command, name="shell_thread")
     shell_thread.start()
     
